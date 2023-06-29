9120ef2e59bee55d400e898ff4a0f662eb2df5ac
prithvijedipatil <prithvipatil09@gmail.com>
2023-06-29 15:22:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,43 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+function randomtime(max,min){
+	return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+funtion generatepromise(time){
+	return new Promise((resolve)=>{
+		setTimeOut(()=>{
+			resolve(`Promise resolved after ${time} seconds`)
+		},time*1000);
+	});
+}
+
+ let promises = [];
+
+for(let i=0;i<5;i++){
+	let randomtime1 = randomtime(5,1);
+	promises.push(generatepromise(randomtime1));
+}
+
+Promise.any((promises).then((data)=>{
+	document.getElementById("output").innerText =data;
+}).catch((error)=>{
+	console.log(error);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
